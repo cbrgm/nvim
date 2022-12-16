a980c786e5bbfc7ffddbc4b5b7d596dd0b351794
cbrgm <chris@cbrgm.net>
2022-12-16 17:29:54 +0100

    update

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -23,7 +23,7 @@ toggleterm.setup({
 	insert_mappings = true, -- whether or not the open mapping applies in insert jode
 	persist_mode = false,
 	persist_size = true,
-	direction = "horizontal", -- 'vertical' | 'horizontal' | 'window' | 'float',
+	direction = "vertical", -- 'vertical' | 'horizontal' | 'window' | 'float',
 	close_on_exit = true, -- close the terminal window when the process exits
 	shell = vim.o.shell, -- change the default shell
 	-- This field is only relevant if direction is set to 'float'
