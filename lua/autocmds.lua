1fcfbe89638555cf44e546ce1cb5eb2edfb2b87b
cbrgm <chris@cbrgm.net>
2023-01-10 14:22:54 +0100

    update

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -52,3 +52,22 @@ autocmd("FileChangedShellPost", {
   group = autoreload_group,
   pattern = "*",
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "qf",
+    "help",
+    "man",
+    "notify",
+    "lspinfo",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "PlenaryTestPopup",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
